90e0eb1c0598a15c70dd32d62780a145de7323bc
Nikolay Viguro <nv@ph-systems.ru>
2015-01-30 17:44:08 +0300

    Speak Services

--- src/ru/iris/terminal/SpeakService.java
+++ src/ru/iris/terminal/SpeakService.java
@@ -2,10 +2,13 @@ package ru.iris.terminal;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
 import android.util.Log;
-import ru.yandex.speechkit.*;
 import ru.yandex.speechkit.Error;
+import ru.yandex.speechkit.*;
 
 import java.util.ArrayList;
 
@@ -13,6 +16,9 @@ public class SpeakService extends Service {
 
     final String TAG = "IRIS-SpeakAndRecognition";
     private Recognizer recognizer;
+    private Vocalizer vocalizer;
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
+    final static int MSG_SPEAK = 0;
 
     public void onCreate() {
         super.onCreate();
@@ -27,10 +33,22 @@ public class SpeakService extends Service {
 
         // Запускаем AVD
         Log.v(TAG, "onStartCommand: Starting AVD");
-        PhraseSpotter.initialize("/smarthome", new AVDListener());
-        PhraseSpotter.start();
+        Error error = PhraseSpotter.initialize("/data/data/ru.iris.terminal/files", new AVDListener());
 
-        someTask();
+        if(error != Error.ERROR_OK)
+        {
+            Log.v(TAG, "Error: " + error.getString());
+        }
+        else {
+            PhraseSpotter.start();
+            Log.v(TAG, "onStartCommand: Started AVD");
+        }
+
+        Log.v(TAG, "Starting Speech");
+        vocalizer = ru.yandex.speechkit.Vocalizer.createVocalizer("ru-RU", "Терминал системы Ирис успешно запущен!", true);
+        vocalizer.setListener(new VocalizerListener());
+        vocalizer.start();
+
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -41,12 +59,64 @@ public class SpeakService extends Service {
         super.onDestroy();
     }
 
+    ////////
+    ////////
+    ////////
+
+    @Override
     public IBinder onBind(Intent intent) {
-        Log.v(TAG, "onBind");
-        return null;
+        return mMessenger.getBinder();
     }
 
-    void someTask() {
+    /**
+     * Handler of incoming messages from clients.
+     * Show Toast with received string
+     */
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_SPEAK:
+                    vocalizer = ru.yandex.speechkit.Vocalizer.createVocalizer("ru-RU", msg.obj.toString(), true);
+                    vocalizer.setListener(new VocalizerListener());
+                    vocalizer.start();
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////////////////
+    // Класс синтеза речи
+    /////////////////////////////////////////////////////////////////////////////
+    private class VocalizerListener implements ru.yandex.speechkit.VocalizerListener {
+
+        final String TAG = "IRIS-Speak";
+
+        public void onSynthesisBegin(Vocalizer vocalizer)
+        {
+            Log.d(TAG, "onSynthesisBegin");
+        }
+
+        public void onSynthesisDone(Vocalizer vocalizer, Synthesis synthesis)
+        {
+            Log.v(TAG, "onSynthesisDone()");
+        }
+
+        public void onPlayingBegin(Vocalizer vocalizer) {
+            Log.v(TAG, "onPlayingBegin()");
+        }
+
+        public void onPlayingDone(Vocalizer vocalizer) {
+            Log.v(TAG, "onPlayingDone()");
+            SpeakService.this.vocalizer = null;
+        }
+
+        public void onVocalizerError(Vocalizer vocalizer, Error error) {
+            Log.v(TAG, "onVocalizerError()");
+            SpeakService.this.vocalizer = null;
+        }
     }
 
     /////////////////////////////////////////////////////////////////////////////
--- src/ru/iris/terminal/TerminalIndex.java
+++ src/ru/iris/terminal/TerminalIndex.java
@@ -2,17 +2,28 @@ package ru.iris.terminal;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
+import android.content.ServiceConnection;
+import android.content.res.AssetManager;
+import android.os.*;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import com.strumsoft.websocket.phonegap.WebSocketFactory;
 
+import java.io.*;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TerminalIndex extends Activity {
 
     private final String TAG = "IRIS-Terminal";
+    private Messenger mService = null;
+    private boolean mBound;
 
     @SuppressLint({"SetJavaScriptEnabled", "AddJavascriptInterface"})
     @Override
@@ -20,6 +31,12 @@ public class TerminalIndex extends Activity {
         super.onCreate(savedInstanceState);
 
         Log.v(TAG, "Start!");
+
+        if (!PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getBoolean("installed", false))
+        {
+            PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit().putBoolean("installed", true).commit();
+            copyAssetFolder(getAssets(), "smarthouse", "/data/data/ru.iris.terminal/files");
+        }
 
         setContentView(R.layout.main);
         WebView wv = (WebView)findViewById(R.id.webview);
@@ -40,8 +57,27 @@ public class TerminalIndex extends Activity {
 
         // Запускаем сервис речи и данные с AMQ
         Log.v(TAG, "Starting services!");
-        startService(new Intent(this, SpeakService.class));
-        startService(new Intent(this, AMQPService.class));
+        Intent speak = new Intent(this, SpeakService.class);
+        Intent amq = new Intent(this, AMQPService.class);
+        bindService(speak, mConnection, Context.BIND_AUTO_CREATE);
+        bindService(amq, mConnection, Context.BIND_AUTO_CREATE);
+        startService(speak);
+        startService(amq);
+        Log.v(TAG, "Starting services done!");
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+
+            public void run() {
+                sendMessage("Говорю всякую хуйню!");
+            }
+        }, 10000);
+
+        timer.schedule(new TimerTask() {
+
+            public void run() {
+                sendMessage("Говорю всякую хуйню еще раз!");
+            }}, 15000 );
     }
 
     @Override
@@ -49,6 +85,102 @@ public class TerminalIndex extends Activity {
     {
         stopService(new Intent(this, SpeakService.class));
         stopService(new Intent(this, AMQPService.class));
+    }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mService = null;
+            mBound = false;
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+
+            Log.v(TAG, "Service connected!");
+
+            mService = new Messenger(service);
+            mBound = true;
+        }
+    };
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Unbind from the service
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
+    }
+
+    public void sendMessage(String text) {
+        if (!mBound)
+            return;
+        Message msg = Message.obtain(null, SpeakService.MSG_SPEAK, text);
+        try {
+            mService.send(msg);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /// Копирование файлов с моделями речи на внутренюю флешку
+    ////////////////////////////////////////////////////////
+
+    private static boolean copyAssetFolder(AssetManager assetManager,
+                                           String fromAssetPath, String toPath) {
+        try {
+            String[] files = assetManager.list(fromAssetPath);
+            new File(toPath).mkdirs();
+            boolean res = true;
+            for (String file : files)
+                if (file.contains("."))
+                    res &= copyAsset(assetManager,
+                            fromAssetPath + "/" + file,
+                            toPath + "/" + file);
+                else
+                    res &= copyAssetFolder(assetManager,
+                            fromAssetPath + "/" + file,
+                            toPath + "/" + file);
+            return res;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static boolean copyAsset(AssetManager assetManager,
+                                     String fromAssetPath, String toPath) {
+
+        Log.v("IRIS", "Copy to " + toPath);
+
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = assetManager.open(fromAssetPath);
+            new File(toPath).createNewFile();
+            out = new FileOutputStream(toPath);
+            copyFile(in, out);
+            in.close();
+            in = null;
+            out.flush();
+            out.close();
+            out = null;
+            return true;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
+        }
     }
 
 }
